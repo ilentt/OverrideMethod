882eb1b2937cea312c6b4e6527df33b0b67806ff
ilentt <ilentt@ilenlab.com>
2015-08-16 16:55:46 +0700

    update unit test

---
+++ OverrideMethod/src/ilentt/ilenlab/com/overridemethod/OverrideMethod.java
@@ -0,0 +1,34 @@
+package ilentt.ilenlab.com.overridemethod;
+
+import org.junit.Test;
+
+public class OverrideMethod {
+
+	@Test
+	public void testMain() {
+		CompareNumberOfMedals.main(new String[] {});
+		
+	}
+	
+	@Test
+	public void testGetMedal() {
+		NumberOfMedals n = new NumberOfMedals(10,20,30);
+		n.getTotalGold();
+		n.getTotalSilver();
+		n.getTotalBronze();
+	}
+	
+	@Test
+	public void testEqual() {
+		NumberOfMedals n = new NumberOfMedals(10,20,30);
+		Object obj = new NumberOfMedals(10,20,30);
+		n.equals(obj);
+		
+		obj = new Object();
+		n.equals(obj);
+		
+		obj = null;
+		n.equals(obj);
+	}
+
+}
